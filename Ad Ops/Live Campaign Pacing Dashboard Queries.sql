c3276d840d661db4ef29f464d6e44d49224ab63e
Nandita Nandakumar <nnandakumar@fubo.tv>
2022-07-05 16:21:35 -0400

    Adding the FW Live campaign details query

---
+++ Ad Ops/Live Campaign Pacing Dashboard Queries.sql
@@ -0,0 +1,4 @@
+-------------------------------------------- Tableau Queries -----------------------
+#NN_Ad_Ops_Freewheel Campaign Pacing
+SELECT DISTINCT t1.*
+FROM `fubotv-prod.dmp.de_sync_freewheel_daily_campaign_pacing` t1
